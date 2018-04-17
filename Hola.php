fd30d76c30f7dafeecfe98ed21d598974a8f74f7
ccortes96 <cristiancortes9645@gmail.com>
2018-04-16 19:35:10 -0600

    jsdf
    adakda

---
+++ Hola.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      echo 'Juan Robando'; 
+      echo 'Juan Fogón';
+    ?>
+  </body>
+</html>
