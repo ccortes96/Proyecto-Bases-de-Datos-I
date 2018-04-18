280328d77ec37cc0372843d689a8e8eda17b7033
ccortes96 <cristiancortes9645@gmail.com>
2018-04-17 21:13:19 -0600

    AJAX
    Finalización de clases ajax

---
+++ Ajax/gestionar-Producto.php
@@ -0,0 +1,30 @@
+<?php
+  include("../class/class-conexion.php");
+  include("../class/class-Producto.php");
+  if(isset($_POST["accion"])){
+    $conexion = new Conexion();
+    switch ($_POST['accion']) {
+      case "listar-todos":
+      break;
+
+      case "seleccionar":
+            break;
+
+      case "eliminar-registro":
+      break;
+
+      case "insertar-registro":
+      break;
+
+      case "actualizar-registro":
+      break;
+
+      default:
+        echo json_encode("Petición inválida");
+      break;
+    }
+    $conexion->cerrarConexion();
+  }else{
+    echo json_encode("No se especificó petición");
+  }
+?>
