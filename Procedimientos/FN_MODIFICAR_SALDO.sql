c550e688d750d0d2a02f45ad19bcd2307dc11291
ccortes96 <cristiancortes9645@gmail.com>
2018-04-19 19:24:37 -0600

    Actualizacion

--- Procedimientos/FN_MODIFICAR_SALDO.sql
+++ Procedimientos/FN_MODIFICAR_SALDO.sql
@@ -1,29 +1,23 @@
 DROP FUNCTION IF EXISTS FN_MODIFICAR_SALDO;
 
 DELIMITER $$
-
-CREATE FUNCTION FN_MODIFICAR_SALDO(
+CREATE FUNCTION `FN_MODIFICAR_SALDO`(
   pnUsuario_idUsuario INT,
   pnIdCuenta          INT,
   pnCantidad          DOUBLE(10,2)
-) RETURNS DOUBLE(10,2)
-
+) RETURNS double(10,2)
 BEGIN
-  DECLARE vnASaldo DOUBLE(10,2);
-  DECLARE vnNSaldo DOUBLE(10,2);
-  SET vnNSaldo = 0;
-  SET vnASaldo = 0;
+  DECLARE vnASaldo DOUBLE(10,2) DEFAULT 0.00;
+  DECLARE vnNSaldo DOUBLE(10,2) DEFAULT 0.00;
 
-  IF F_VERIFICAR_USUARIO(pnUsuario_idUsuario) = 0 THEN
-    LEAVE;
-  ELSE
-    SET vnASaldo = SELECT Saldo FROM Cuenta
+  IF F_VERIFICAR_USUARIO(pnUsuario_idUsuario) = 1 THEN
+    SELECT Saldo INTO vnASaldo FROM Cuenta
     WHERE idCuenta = pnIdCuenta AND Usuario_idUsuario = pnUsuario_idUsuario;
+      SET vnNSaldo = (vnASaldo + pnCantidad);
+      RETURN vnNSaldo;
+  ELSE
+  	RETURN vnNSaldo;
   END IF;
 
-  SET vnNSaldo = (vnASaldo + pnCantidad)
-
-  RETURN vnNSaldo;
-
-  END $$
-  DELIMITER ;
+  END$$
+DELIMITER ;
--- Procedimientos/SP_LOGIN.sql
+++ Procedimientos/SP_LOGIN.sql
@@ -3,17 +3,17 @@ DROP PROCEDURE IF EXISTS SP_LOGIN;
 DELIMITER $$
 
 CREATE PROCEDURE SP_LOGIN(
-  IN pcCorreoUsuario       VARCHAR(45),
-  IN pcContrasenia         VARCHAR(45),
-  OUT pcMensaje   		     VARCHAR(1000),
-  OUT pbOcurrioError 	     BOOLEAN
+  IN pcCorreoUsuario       	VARCHAR(45),
+  IN pcContrasenia         	VARCHAR(45),
+  OUT pcMensaje   		    VARCHAR(1000),
+  OUT pbOcurrioError 	    BOOLEAN
 )
 
 SP: BEGIN
-  DECLARE vnConteo  INT;
-  DECLARE vnUsuario INT;
-  DECLARE vnCuenta  INT;
-  DECLARE vcContrasenia  VARCHAR(45);
+  DECLARE vnConteo  	INT DEFAULT 0;
+  DECLARE vnUsuario 	INT;
+  DECLARE vnCuenta  	INT;
+  DECLARE vcContrasenia VARCHAR(45);
   SET autocommit=0;
 
   /*START TRANSACTION;*/
@@ -21,7 +21,7 @@ SP: BEGIN
   SET pcMensaje='';
   SET pbOcurrioError=TRUE;
 
-  SELECT COUNT(*) INTO vnConteo FROM CorreoUsuario cu
+  SELECT COUNT(*) INTO vnConteo FROM correousuario cu
   WHERE cu.descripcion = pcCorreoUsuario;
 
   IF vnConteo = 0 THEN
@@ -29,15 +29,15 @@ SP: BEGIN
     LEAVE SP;
 
   ELSE
-    SET vnUsuario = SELECT us.idUsuario FROM
+    SELECT us.idUsuario INTO vnUsuario FROM
     (Usuario us INNER JOIN CorreoUsuario cu ON us.idUsuario = cu.Usuario_idUsuario)
     WHERE cu.descripcion = pcCorreoUsuario;
 
-    SET vnCuenta = SELECT cta.idCuenta FROM
+    SELECT cta.idCuenta INTO vnCuenta FROM
     (Usuario us INNER JOIN Cuenta cta ON us.idUsuario = cta.Usuario_idUsuario)
     WHERE cta.Usuario_idUsuario = vnUsuario;
 
-    SET vcContrasenia = SELECT cta.contrasenia FROM Cuenta cta
+    SELECT cta.contrasenia INTO vcContrasenia FROM Cuenta cta
     WHERE cta.idCuenta = vnCuenta;
 
       IF (vcContrasenia = pcContrasenia) THEN
